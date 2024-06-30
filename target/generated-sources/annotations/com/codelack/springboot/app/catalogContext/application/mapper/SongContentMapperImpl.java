bea9658e26deca36ae494e071783159cbd37f773
Lack01 <juan.09.molina@gmail.com>
2024-06-30 21:00:12 +0100

    config docker

---
+++ target/generated-sources/annotations/com/codelack/springboot/app/catalogContext/application/mapper/SongContentMapperImpl.java
@@ -0,0 +1,13 @@
+package com.codelack.springboot.app.catalogContext.application.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-30T20:54:49+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class SongContentMapperImpl implements SongContentMapper {
+}
---
+++ target/generated-sources/annotations/com/codelack/springboot/app/usercontext/mapper/UserMapperImpl.java
@@ -0,0 +1,36 @@
+package com.codelack.springboot.app.usercontext.mapper;
+
+import com.codelack.springboot.app.usercontext.ReadUserDTO;
+import com.codelack.springboot.app.usercontext.domain.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-30T20:54:49+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public ReadUserDTO readUserDTOToUser(User entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        String firstName = null;
+        String lastName = null;
+        String email = null;
+        String imageUrl = null;
+
+        firstName = entity.getFirstName();
+        lastName = entity.getLastName();
+        email = entity.getEmail();
+        imageUrl = entity.getImageUrl();
+
+        ReadUserDTO readUserDTO = new ReadUserDTO( firstName, lastName, email, imageUrl );
+
+        return readUserDTO;
+    }
+}
